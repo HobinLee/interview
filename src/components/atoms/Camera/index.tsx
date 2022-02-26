005e775feff3dbf4b31ffad0f902838242846ada
HobinLee <viny5120@gmail.com>
2022-02-26 11:32:12 +0900

    feat: 녹화 성공

--- src/components/atoms/Camera/index.tsx
+++ src/components/atoms/Camera/index.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState, VFC } from 'react';
 import Webcam from 'react-webcam';
 import { useRecordWebcam } from 'react-record-webcam';
 import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { standbyState } from '@src/stores/interview';
 
 interface CameraProps {
   record?: boolean;
@@ -11,17 +13,35 @@ interface CameraProps {
 
 const Camera: VFC<CameraProps> = ({ record }) => {
   const recordWebcam = useRecordWebcam();
+  const standby = useRecoilValue(standbyState);
+
   useEffect(() => {
     record && recordWebcam.open();
   }, []);
 
+  useEffect(() => {
+    if (standby) {
+      if (recordWebcam.status === 'RECORDING') recordWebcam.stop();
+    } else {
+      recordWebcam.start();
+    }
+  }, [standby]);
+
+  useEffect(() => {
+    if (recordWebcam.status === 'PREVIEW') {
+      saveFile();
+    }
+  }, [recordWebcam.status]);
+
   const saveFile = async () => {
     const blob = await recordWebcam.getRecording();
+    console.log(blob);
+    recordWebcam.open();
   };
 
   if (!record) return <CameraWrap autoPlay muted />;
 
-  return <CameraWrap as="video" ref={recordWebcam.webcamRef} autoPlay muted />;
+  // return <CameraWrap as="video" ref={recordWebcam.webcamRef} autoPlay muted />;
   return (
     <div>
       <p>Camera status: {recordWebcam.status}</p>
@@ -32,7 +52,6 @@ const Camera: VFC<CameraProps> = ({ record }) => {
       <button onClick={recordWebcam.download}>Download recording</button>
       <button onClick={saveFile}>Save file to server</button>
       <video ref={recordWebcam.webcamRef} autoPlay muted />
-      <video ref={recordWebcam.previewRef} autoPlay muted loop />
     </div>
   );
 };
